59f1b9716d40edd0030fdb6191a9d60e0eba1fd5
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-02-11 10:43:22 +0800

    Update __init__.py

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,1 +1,6 @@
+from .schools import SchoolCrawler
+from .majors import MajorCrawler
+from .scores import ScoreCrawler
+from .plans import PlanCrawler
 
+__all__ = ['SchoolCrawler', 'MajorCrawler', 'ScoreCrawler', 'PlanCrawler']
